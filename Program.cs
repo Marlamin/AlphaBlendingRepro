aa403accd9c6550b904409e3e244a03837516f06
Martin Benjamins <marlamin@marlamin.com>
2018-04-22 20:58:42 +0200

    Add code/images

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace GDIAlphaBlendRepro
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var bg = new Bitmap(Image.FromFile("background.png"));
+
+            var g = Graphics.FromImage(bg);
+            g.DrawImage(new Bitmap(Image.FromFile("layer.png")), 50, 50);
+
+            bg.Save("result.png");
+        }
+    }
+}
